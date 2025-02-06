7ad859bc0a2f0e35a001577514f02aea0ed211be
popote1 <yvan.bladet@gmail.com>
2025-02-06 12:11:47 +0100

    FirstPush

---
+++ Assets/Script/Projectile.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour {
+    public float Speed = 10;
+    public int Damage = 1;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Transform _visual;
+    void Start() {
+        _rigidbody.AddForce(new Vector3(Speed,0,0), ForceMode.Impulse);
+    }
+
+    private void Update() {
+        if (_visual == null) return;
+        _visual.right = -_rigidbody.linearVelocity;
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.GetComponent<IDamagable>() != null) {
+            other.gameObject.GetComponent<IDamagable>().TakeDamage(Damage);
+        }
+        Destroy(gameObject);
+    }
+}
